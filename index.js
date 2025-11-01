0c984b70963a0a9f902f4f4dac6f2aadb4d850b3
Muhammadamin <muhamadamin1992@gmail.com>
2025-11-01 18:58:00 +0500

    feat: print homedir path when start

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+import { homedir } from "node:os";
 import { parseArgs } from "node:util";
 
-const { values: { username } } = parseArgs({ options: {
-  username: {
-    type: "string",
-  }
-}});
+const {
+  values: { username },
+} = parseArgs({
+  options: {
+    username: {
+      type: "string",
+    },
+  },
+});
+
+let currentDir = homedir();
+
+const printCurrentDir = () => {
+  console.log(`You are currently in ${currentDir}`);
+};
+
+process.stdin.setDefaultEncoding("utf-8").on("data", (data) => {
+  console.log(data.toString());
+});
 
 console.log(`Welcome to the File Manager, ${username}!`);
+printCurrentDir();
