4d264bb112980fed84b54fef6e0de2abb2dabaa7
Muhammadamin <muhamadamin1992@gmail.com>
2025-11-01 19:46:49 +0500

    feat: implement ls command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { homedir } from "node:os";
 import { resolve } from "node:path";
 import { parseArgs } from "node:util";
+import { readdir } from "node:fs/promises";
 
 const {
   values: { username },
@@ -22,8 +23,33 @@ const commands = {
   up: () => {
     currentDir = resolve(currentDir, "..");
     printCurrentDir();
-  }
-}
+  },
+  ls: async () => {
+    console.table(
+      (
+        await readdir(currentDir, {
+          withFileTypes: true,
+        })
+      )
+        .map((dirent) => ({
+          name: dirent.name,
+          type: dirent.isDirectory() ? "directory" : "file",
+        }))
+        .toSorted((a, b) => {
+          if (a.type > b.type) {
+            return 1;
+          }
+          if (a.type < b.type) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return -1;
+        })
+    );
+  },
+};
 
 process.stdin.setDefaultEncoding("utf-8").on("data", (data) => {
   const input = data.toString().trim();
